24165e79f771b12c144326b6efd689fcc0b27862
federicoacker <acker.federico@gmail.com>
2026-06-04 14:00:42 +0200

    Fatta la seconda query

--- soluzioni1.sql
+++ soluzioni1.sql
@@ -5,3 +5,10 @@ SELECT `s`.*
 FROM `students` `s`
 WHERE YEAR(`s`.`date_of_birth`) = 1990;
 
+/*
+2. Selezionare tutti i corsi he valgono più di 10 crediti
+*/
+SELECT `c`.`name`, `c`.`cfu`,`c`.`year`
+FROM `courses` `c`
+WHERE `c`.`cfu` > 10
+ORDER BY `c`.`cfu`, `c`.`year`;
